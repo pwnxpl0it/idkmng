d8821c01a3e8b3450033bb0789740fd8706f32ba
0xr00t3d <mohamednafagr@gmail.com>
2023-03-07 13:43:32 +0200

    added funcs.rs

---
+++ src/funcs.rs
@@ -0,0 +1,8 @@
+use std::io;
+
+pub fn read(name: String) -> String {
+    println!("{}:",name);
+    let mut input = String::from("");
+    io::stdin().read_line(&mut input).unwrap();
+    input.trim().to_string()
+}
