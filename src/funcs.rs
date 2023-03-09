53bd235b12c6d9583486badeea3d3192d3a1f4b4
0xr00t3d <mohamednafagr@gmail.com>
2023-03-09 19:55:46 +0200

    don't print function name

--- src/funcs.rs
+++ src/funcs.rs
@@ -1,7 +1,9 @@
 use std::io;
 
 pub fn read(name: String) -> String {
-    println!("{}:",name);
+    println!("{}:",name.replace(
+            ":read",""
+            ));
     let mut input = String::from("");
     io::stdin().read_line(&mut input).unwrap();
     input.trim().to_string()
